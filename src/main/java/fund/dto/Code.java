2457f5a822144967b4491619a46e091125321eb8
JIEUNAN <jea0411op@gmail.com>
2016-08-24 13:16:16 +0900

    dto commitment,commitmentDetail,code,corporate

---
+++ src/main/java/fund/dto/Code.java
@@ -0,0 +1,50 @@
+package fund.dto;
+
+public class Code {
+		int ID;
+		String codeGroup;
+		String codeName;
+		String etc1;
+		String etc2;
+		String etc3;
+		
+		public int getID() {
+			return ID;
+		}
+		public void setID(int iD) {
+			ID = iD;
+		}
+		public String getCodeGroup() {
+			return codeGroup;
+		}
+		public void setCodeGroup(String codeGroup) {
+			this.codeGroup = codeGroup;
+		}
+		public String getCodeName() {
+			return codeName;
+		}
+		public void setCodeName(String codeName) {
+			this.codeName = codeName;
+		}
+		public String getEtc1() {
+			return etc1;
+		}
+		public void setEtc1(String etc1) {
+			this.etc1 = etc1;
+		}
+		public String getEtc2() {
+			return etc2;
+		}
+		public void setEtc2(String etc2) {
+			this.etc2 = etc2;
+		}
+		public String getEtc3() {
+			return etc3;
+		}
+		public void setEtc3(String etc3) {
+			this.etc3 = etc3;
+		}
+		
+		
+		
+}
