76d113a015e9690053ffcadeb6f400f4624a9bb8
ruuu <norlights23@gmail.com>
2016-09-26 14:58:20 +0900

    [임시] initialize PaymentController

--- src/main/java/fund/controller/PaymentController.java
+++ src/main/java/fund/controller/PaymentController.java
@@ -2,13 +2,9 @@ package fund.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import fund.mapper.PaymentMapper;
-import fund.dto.*;
+
 
 @Controller
 public class PaymentController {
