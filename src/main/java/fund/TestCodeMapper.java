30795702f0dec3ce63cd274008479c41c27e14f1
JIEUNAN <jea0411op@gmail.com>
2016-11-04 18:42:21 +0900

    [junit]약정

---
+++ src/main/java/fund/TestCodeMapper.java
@@ -0,0 +1,11 @@
+package fund;
+
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+public class TestCodeMapper {
+
+}
